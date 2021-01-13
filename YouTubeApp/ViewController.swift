a6aff3a38990d3841d04c25954919eeb36786c5e
ArtemUstinov <arti-0@mail.ru>
2021-01-13 12:12:00 +0300

    Initial Commit

---
+++ YouTubeApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YouTubeApp
+//
+//  Created by Артём Устинов on 13.01.2021.
+//  Copyright © 2021 Artem Ustinov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
